8c7a928712f354eb94ac018a9bad6fedfdae5098
qwexfour <deman7866@mail.ru>
2019-06-21 10:40:21 +0300

    node_base_t now inherits pass_t, not contains. Being so we don't have to store pointer to node in pass, node now is 2 pointers lighter.

--- forest.cpp
+++ forest.cpp
@@ -1,43 +1,64 @@
 #include "forest.hpp"
 
-using namespace forestlib;
+#include <cassert>
 
-detail::pass_t::type_t detail::opposite_pass_type(detail::pass_t::type_t type) noexcept
+using namespace forestlib;
+using namespace detail;
+
+pass_base_t::type_t detail::opposite_pass_type(pass_base_t::type_t type) noexcept
 {
     switch (type)
     {
-        case detail::pass_t::type_t::LEAD:
-            return detail::pass_t::type_t::TAIL;
+        case pass_base_t::type_t::LEAD:
+            return pass_base_t::type_t::TAIL;
 
-        case detail::pass_t::type_t::TAIL:
-            return detail::pass_t::type_t::LEAD;
+        case pass_base_t::type_t::TAIL:
+            return pass_base_t::type_t::LEAD;
 
         default:
             assert(0 && "wrong argument");
     }
 }
 
-detail::node_base_t* detail::traverse(detail::node_base_t* node,
-                                      detail::pass_t::type_t traversal,
-                                      detail::pass_t::direction_t dir) noexcept
+node_base_t* detail::traverse(node_base_t* node,
+                              pass_base_t::type_t traversal,
+                              pass_base_t::direction_t dir) noexcept
 {
-    auto consider_type = traversal;
-    auto opposite_type = detail::opposite_pass_type(consider_type);
+    auto considered_type = traversal;
+    auto opposite_type = opposite_pass_type(considered_type);
 
     // passing edges leading to opposite pass
-    auto cur_pass = node->get(consider_type).get(dir);
+    auto cur_pass = node->get_pass(considered_type).get(dir);
     for (;cur_pass->type_ == opposite_type;
          cur_pass = cur_pass->get(dir));
-    
-    assert(cur_pass->type_ == consider_type);
-    return cur_pass->node_;
+
+    assert(cur_pass->type_ == considered_type);
+    return get_node(cur_pass);
 }
 
-const detail::node_base_t* detail::traverse(const detail::node_base_t* node,
-                                            detail::pass_t::type_t traversal,
-                                            detail::pass_t::direction_t dir) noexcept
+const node_base_t* detail::traverse(const node_base_t* node,
+                                    pass_base_t::type_t traversal,
+                                    pass_base_t::direction_t dir) noexcept
 {
-    // I know what I'm doing (just like in stl_tree from std::map)
-    return detail::traverse(const_cast<detail::node_base_t*>(node),
-                            traversal, dir);
+    return traverse(const_cast<node_base_t*>(node), traversal, dir);
+}
+
+const node_base_t* detail::get_node(const pass_base_t* pass) noexcept
+{
+    if (pass->type_ == pass_base_t::type_t::LEAD)
+    {
+        return static_cast<const node_base_t*>(
+            static_cast<const pass_t<pass_base_t::type_t::LEAD>*>(pass));
+    }
+    else
+    {
+        assert(pass->type_ == pass_base_t::type_t::TAIL);
+        return static_cast<const node_base_t*>(
+            static_cast<const pass_t<pass_base_t::type_t::TAIL>*>(pass));
+    }
+}
+
+node_base_t* detail::get_node(pass_base_t* pass) noexcept
+{
+    return const_cast<node_base_t*>(get_node(static_cast<const pass_base_t*>(pass)));
 }
